454d478a99c24e2cfc26edf12ac2b7f737c47056
Caio042 <caiohenriquegomeslima@gmail.com>
2021-08-15 19:25:16 -0300

    Estrutura inicial do projeto

---
+++ src/test/java/com/caiolima/apientrega/ApiEntregaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caiolima.apientrega;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiEntregaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
